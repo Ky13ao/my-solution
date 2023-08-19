51b11426338ece76235eb1ef675b0d250b515837
Bao DK <bao_dk@jabil.com>
2023-08-19 09:47:36 +0700

    commit 2023-08-20

--- DBI202x.3.0.VN/lab4.sql
+++ DBI202x.3.0.VN/lab4.sql
@@ -6,13 +6,13 @@ SOURCE book_data.sql;
 SELECT * FROM books;
 
 -- Lab4.2: https://docs.google.com/document/d/1jbHhMOR61CoD79D_r3SdN6et5UrG3tAE/edit
-SELECT CONCAT(author_fname, author_lname) AS 'full_name' FROM books; --> fname lname
-SELECT CONCAT_WS(' - ',author_fname, author_lname) AS 'full_name' FROM books; --> fname - lname
-SELECT SUBSTRING('data analyst', 6, 7) s; --> analyst
-SELECT REPLACE('data analyst', 'analyst', 'science') s; --> data science
-SELECT REVERSE('data analyst'); --> tsylana atad
-SELECT CHAR_LENGTH('database') l; --> 8
-SELECT UPPER('database') s; --> DATABASE
+SELECT CONCAT(author_fname, author_lname) AS 'full_name' FROM books; -- > fname lname
+SELECT CONCAT_WS(' - ',author_fname, author_lname) AS 'full_name' FROM books; -- > fname - lname
+SELECT SUBSTRING('data analyst', 6, 7) s; -- > analyst
+SELECT REPLACE('data analyst', 'analyst', 'science') s; -- > data science
+SELECT REVERSE('data analyst'); -- > tsylana atad
+SELECT CHAR_LENGTH('database') l; -- > 8
+SELECT UPPER('database') s; -- > DATABASE
 SELECT UPPER(b.title) upperTitle FROM books b;
-SELECT LOWER('DATABASE') s; --> database
+SELECT LOWER('DATABASE') s; -- > database
 SELECT LOWER(b.title) lowerTitle FROM books b;
